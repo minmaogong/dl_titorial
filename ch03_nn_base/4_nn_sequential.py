595c4abac942e54b410d3eba36dedba025be0d34
maomin <1220826363@qq.com>
2026-07-12 21:48:11 +0800

    ïnew: 神经网络基础-搭建神经网络-Sequentialâ

---
+++ ch03_nn_base/4_nn_sequential.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+from torchsummary import summary
+
+# 1. 输入数据
+X = torch.randn(10, 3) # 生成一个10行3列的随机张量，表示10条数据，每条数据3个特征
+
+# 2. 定义模型
+model = nn.Sequential(
+    nn.Linear(3, 4),
+    nn.Tanh(), # 激活函数-双曲正切
+    nn.Linear(4, 4),
+    nn.ReLU(), # 激活函数-修正线性单元
+    nn.Linear(4, 2),
+    nn.Softmax(dim=-1) # 激活函数-Softmax dim=-1 表示最后一个维度，Softmax将一个任意的实数向量转换为一个概率分布，确保输出值的总和为1
+)
+
+# 前向传播
+y = model(X)
+print(y)
+
+summary(model, input_size=(3, ), batch_size=10, device='cpu')
